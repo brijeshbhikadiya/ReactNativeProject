4d56bb3bad5f9c02d96a2974c24f590ee333aa5a
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2024-02-20 10:36:44 +0530

    Task_12 done with all functionalities and font family added
    Signed-off-by: brijeshbhikadiya <brijeshbhikadiya27@gmail.com>

--- Task_12/index.js
+++ Task_12/index.js
@@ -9,6 +9,7 @@ import {name as appName} from './app.json';
 import OnBording from './src/screen/OnBording';
 import Login from './src/screen/Login';
 import SignUp from './src/screen/SignUp';
+import AppNavigator from './src/screen/OnBording';
 
 
 
@@ -16,4 +17,4 @@ import SignUp from './src/screen/SignUp';
 
 
 
- AppRegistry.registerComponent(appName, () => OnBording);
+ AppRegistry.registerComponent(appName, () => AppNavigator);
