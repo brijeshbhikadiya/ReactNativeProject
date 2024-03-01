c6c7d807f51ac458202a9f443b0ccfcb126267b0
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2024-03-01 15:07:36 +0530

    Task_16 Completed
    Signed-off-by: brijeshbhikadiya <brijeshbhikadiya27@gmail.com>

---
+++ Task_16/src/screen/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    )
+  }
+}
