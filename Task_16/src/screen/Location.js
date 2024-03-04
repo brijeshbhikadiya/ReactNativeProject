96e6e47a31ef5d4c13d07fd1deefd31d2f7ced76
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2024-03-04 10:22:47 +0530

    Task_19 Created
    Signed-off-by: brijeshbhikadiya <brijeshbhikadiya27@gmail.com>

--- Task_16/src/screen/Location.js
+++ Task_16/src/screen/Location.js
@@ -1,194 +1,218 @@
-import { Alert, FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { Component } from 'react'
-
+import {
+  Alert,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {Component} from 'react';
 
 export default class HomeScreen extends Component {
-    constructor(){
-        super();
-        this.state={
-            teamData:[
-            {
-                profileImage:require('../assets/images/Venue5.png'),
-                teamName:"Football Venue",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue6.png'),
-                teamName:"Fooball Space",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue1.png'),
-                teamName:"Football Venue",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue2.png'),
-                teamName:"Venue 1",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue3.png'),
-                teamName:"Venue 2",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue4.png'),
-                teamName:"Venue 3",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue5.png'),
-                teamName:"Venue 4",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            },
-            {
-                profileImage:require('../assets/images/Venue6.png'),
-                teamName:"Venue 5",
-                playerCount:"San Francisco",
-                deleteImage:require('../assets/images/Delete.png')
-            }
-        ],
-        itemCount:0
-        }
-        
-    }
+  constructor() {
+    super();
+    this.state = {
+      teamData: [
+        {
+          profileImage: require('../assets/images/Venue5.png'),
+          teamName: 'Football Venue',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue6.png'),
+          teamName: 'Fooball Space',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue1.png'),
+          teamName: 'Football Venue',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue2.png'),
+          teamName: 'Venue 1',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue3.png'),
+          teamName: 'Venue 2',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue4.png'),
+          teamName: 'Venue 3',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue5.png'),
+          teamName: 'Venue 4',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+        {
+          profileImage: require('../assets/images/Venue6.png'),
+          teamName: 'Venue 5',
+          playerCount: 'San Francisco',
+          deleteImage: require('../assets/images/Delete.png'),
+        },
+      ],
+      itemCount: 0,
+    };
+  }
 
-    componentDidMount = () => {
+  componentDidMount = () => {
+    this.updateItemCount();
+  };
+
+  handleDelete = index => {
+    Alert.alert(
+      'Confirmation',
+      'Are you sure you want to delete This Team',
+      [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'Delete', onPress: () => this.deleteItem(index)},
+      ],
+      {cancelable: false},
+    );
+  };
+
+  deleteItem = item => {
+    const updatedTeams = this.state.teamData.filter(team => team !== item);
+    this.setState(
+      {
+        teamData: updatedTeams,
+      },
+      () => {
         this.updateItemCount();
-    }
+      },
+    );
+  };
 
-    handleDelete = (index) => {
-        Alert.alert(
-            'Confirmation',
-            'Are you sure you want to delete This Team',
-            [{text:'Cancel',style:'cancel'},{text:'Delete',onPress:()=> this.deleteItem(index)}],{cancelable:false}
-        );
-    }
-
-    deleteItem = (item) => {
-        const updatedTeams = this.state.teamData.filter((team)=>team !== item);
-        this.setState({
-            teamData:updatedTeams,
-        },() => {
-            this.updateItemCount();
-        })
-    }
-
-    updateItemCount = () => {
-        const itemCount = this.state.teamData.length;
-        this.setState({
-            itemCount
-        });
-    }
+  updateItemCount = () => {
+    const itemCount = this.state.teamData.length;
+    this.setState({
+      itemCount,
+    });
+  };
   render() {
     return (
       <View style={styles.Container}>
         <ScrollView>
-      
+          {/* <View style={styles.LineView}></View> */}
 
-        {/* <View style={styles.LineView}></View> */}
-
-        <View style={styles.InnerContainer}>
-            {this.state.itemCount==1 || this.state.itemCount==0?<Text style={styles.TeamsCountText}>Venue ({`${this.state.itemCount}`})</Text>:<Text style={styles.TeamsCountText}>Venues ({`${this.state.itemCount}`})</Text>}
+          <View style={styles.InnerContainer}>
+            {this.state.itemCount == 1 || this.state.itemCount == 0 ? (
+              <Text style={styles.TeamsCountText}>
+                Venue ({`${this.state.itemCount}`})
+              </Text>
+            ) : (
+              <Text style={styles.TeamsCountText}>
+                Venues ({`${this.state.itemCount}`})
+              </Text>
+            )}
             <FlatList
-            scrollEnabled={false}
-            data={this.state.teamData}
-            renderItem={({item,index})=>
+              scrollEnabled={false}
+              data={this.state.teamData}
+              renderItem={({item, index}) => (
                 <View style={styles.FlatlistView}>
-                    <Image source={item.profileImage}></Image>
-                    <View style={{marginLeft:10,marginTop:2}}>
-                        <Text style={styles.TeamNameText}>{item.teamName}</Text>
-                        <Text style={styles.TeamCountText}>{item.playerCount}</Text>
-                    </View>
-                    <TouchableOpacity onPress={()=>this.handleDelete(item)} style={styles.DeleteImage}>
-                        <Image source={item.deleteImage} ></Image>
-                    </TouchableOpacity>
+                  <Image source={item.profileImage}></Image>
+                  <View style={{marginLeft: 10, marginTop: 2}}>
+                    <Text style={styles.TeamNameText}>{item.teamName}</Text>
+                    <Text style={styles.TeamCountText}>{item.playerCount}</Text>
+                  </View>
+                  <TouchableOpacity
+                    onPress={() => this.handleDelete(item)}
+                    style={styles.DeleteImage}>
+                    <Image source={item.deleteImage}></Image>
+                  </TouchableOpacity>
                 </View>
-            }
-            ></FlatList>
-        </View>
-        
+              )}></FlatList>
+          </View>
         </ScrollView>
-        <Image source={require('../assets/images/Plus.png')} style={{alignSelf:'center',marginTop:-30,marginBottom:20}}></Image>
+        <Image
+          source={require('../assets/images/Plus.png')}
+          style={{
+            alignSelf: 'center',
+            marginTop: -30,
+            marginBottom: 20,
+          }}></Image>
       </View>
-      
-    )
+    );
   }
 }
 const styles = StyleSheet.create({
-    Container:{
-        flex:1,
-        backgroundColor:'rgba(255, 255, 255, 1)'
-    },
-    InnerContainer:{
-        backgroundColor:'rgba(246, 249, 252, 1)',
-        paddingBottom:67
-    },
-    TitleBarView:{
-        flexDirection:'row',
-        justifyContent:'center',
-        marginTop:20
-    },
-    FlatlistView:{
-        flexDirection:'row',
-        backgroundColor:'rgba(255, 255, 255, 1)',
-        marginHorizontal:20,
-        marginTop:5,
-        marginBottom:5,
-        borderRadius:10,
-        shadowColor:'black',
-        shadowOpacity:0.5,
-        elevation:2,
-        paddingVertical:8.5,
-        paddingLeft:13.05,
-        paddingRight:15.5,
-    },
-    LineView:{
-        marginTop:17,
-        borderColor:'rgba(180, 180, 183, 0.5)',
-        borderWidth:1
-    },
-    MoreImage:{
-        position:'absolute',
-        left:20
-
-    },
-    SearchImage:{
-        position:'absolute',
-        right:20
-    },
-    DeleteImage:{
-        position:'absolute',
-        right:15.5,
-        top:16.91
-    },
-    TeamsCountText:{
-        marginTop:10,
-        marginBottom:8,
-        marginLeft:20,
-        color:'rgba(0, 0, 0, 1)',
-        fontWeight:'500',
-        fontSize:16,
-        lineHeight:19.49
-    },
-    TeamNameText:{
-        color:'rgba(0, 0, 0, 1)',
-        fontWeight:'500',
-        fontSize:14,
-        lineHeight:17.05
-    },
-    TeamCountText:{
-        color:'rgba(0, 0, 0, 0.5)',
-        fontWeight:'400',
-        fontSize:13,
-        //lineHeight:3
-    }
-})
+  Container: {
+    flex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+  InnerContainer: {
+    backgroundColor: 'rgba(246, 249, 252, 1)',
+    paddingBottom: 67,
+  },
+  TitleBarView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  FlatlistView: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    marginHorizontal: 20,
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    elevation: 2,
+    paddingVertical: 8.5,
+    paddingLeft: 13.05,
+    paddingRight: 15.5,
+  },
+  LineView: {
+    marginTop: 17,
+    borderColor: 'rgba(180, 180, 183, 0.5)',
+    borderWidth: 1,
+  },
+  MoreImage: {
+    position: 'absolute',
+    left: 20,
+  },
+  SearchImage: {
+    position: 'absolute',
+    right: 20,
+  },
+  DeleteImage: {
+    position: 'absolute',
+    right: 15.5,
+    top: 16.91,
+  },
+  TeamsCountText: {
+    marginTop: 10,
+    marginBottom: 8,
+    marginLeft: 20,
+    color: 'rgba(0, 0, 0, 1)',
+    fontWeight: '500',
+    fontSize: 16,
+    lineHeight: 19.49,
+  },
+  TeamNameText: {
+    color: 'rgba(0, 0, 0, 1)',
+    fontWeight: '500',
+    fontSize: 14,
+    lineHeight: 17.05,
+  },
+  TeamCountText: {
+    color: 'rgba(0, 0, 0, 0.5)',
+    fontWeight: '400',
+    fontSize: 13,
+    //lineHeight:3
+  },
+});
--- Task_16/src/screen/New.js
+++ Task_16/src/screen/New.js
@@ -1,8 +1,8 @@
-import { Text, View ,StyleSheet,Image} from 'react-native'
-import React, { Component } from 'react'
+import {Text, View, StyleSheet, Image} from 'react-native';
+import React, {Component} from 'react';
 import HomeScreen from './HomeScreen';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import Players from './Players';
 import Location from './Location';
 import Turnaments from './Turnaments';
@@ -12,123 +12,141 @@ const Tab = createMaterialTopTabNavigator();
 export default class New extends Component {
   render() {
     return (
-       <View style={{flex:1,backgroundColor:'white'}}>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
         <View style={styles.TitleBarView}>
-            <Image source={require('../assets/images/More.png')} style={styles.MoreImage}></Image>
-            <Image source={require('../assets/images/TournamentPic.png')}></Image>
-            <Image source={require('../assets/images/Search.png')} style={styles.SearchImage}></Image>
-            </View>
+          <Image
+            source={require('../assets/images/More.png')}
+            style={styles.MoreImage}></Image>
+          <Image source={require('../assets/images/TournamentPic.png')}></Image>
+          <Image
+            source={require('../assets/images/Search.png')}
+            style={styles.SearchImage}></Image>
+        </View>
 
-            <View style={styles.LineView}></View>
+        <View style={styles.LineView}></View>
 
         <NavigationContainer>
-        <Tab.Navigator initialRouteName='Turnaments' screenOptions={({route}) => ({
-            tabBarIcon:({focused,color,size}) => {
-              let iconName;
+          <Tab.Navigator
+            initialRouteName="Turnaments"
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName;
 
-              if(route.name === 'Players'){
-                iconName = focused ? require('../assets/images/PlayerYellow.png') : require('../assets/images/Player.png');
+                if (route.name === 'Players') {
+                  iconName = focused
+                    ? require('../assets/images/PlayerYellow.png')
+                    : require('../assets/images/Player.png');
+                } else if (route.name === 'HomeScreen') {
+                  iconName = focused
+                    ? require('../assets/images/TeamPlayerYellow.png')
+                    : require('../assets/images/TeamPlayer.png');
+                } else if (route.name === 'Location') {
+                  iconName = focused
+                    ? require('../assets/images/LocationYellow.png')
+                    : require('../assets/images/Location.png');
+                } else {
+                  iconName = focused
+                    ? require('../assets/images/TrophyYellow.png')
+                    : require('../assets/images/Trophy.png');
+                }
 
-              }
-              else if(route.name === 'HomeScreen'){
-                iconName = focused ? require('../assets/images/TeamPlayerYellow.png') : require('../assets/images/TeamPlayer.png');
-              }
-              else if(route.name === 'Location'){
-                iconName = focused ? require('../assets/images/LocationYellow.png') : require('../assets/images/Location.png');
-              }
-              else {
-                iconName = focused? require('../assets/images/TrophyYellow.png') : require('../assets/images/Trophy.png');
-              }
-
-              return <Image source={iconName} style={{marginTop:15}}/>
-            },
-            tabBarIndicatorStyle:{
-              backgroundColor:null
-            }
-          })}>
-
-
-            <Tab.Screen name="Turnaments" component={Turnaments}  options={{tabBarLabel:''}}/>
-            <Tab.Screen name="HomeScreen" component={HomeScreen}  options={{tabBarLabel:''}}/>
-            <Tab.Screen name="Players" component={Players}  options={{tabBarLabel:''}}/>
-             <Tab.Screen name="Location" component={Location}  options={{tabBarLabel:''}}/>
-             
-
-         </Tab.Navigator>
-
-         </NavigationContainer>
-       
-         </View>
-        
-    )
+                return <Image source={iconName} style={{marginTop: 15}} />;
+              },
+              tabBarIndicatorStyle: {
+                backgroundColor: null,
+              },
+            })}>
+            <Tab.Screen
+              name="Turnaments"
+              component={Turnaments}
+              options={{tabBarLabel: ''}}
+            />
+            <Tab.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{tabBarLabel: ''}}
+            />
+            <Tab.Screen
+              name="Players"
+              component={Players}
+              options={{tabBarLabel: ''}}
+            />
+            <Tab.Screen
+              name="Location"
+              component={Location}
+              options={{tabBarLabel: ''}}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
-    Container:{
-        flex:1,
-        backgroundColor:'rgba(255, 255, 255, 1)'
-    },
-    InnerContainer:{
-        backgroundColor:'rgba(246, 249, 252, 1)',
-        paddingBottom:67
-    },
-    TitleBarView:{
-        flexDirection:'row',
-        justifyContent:'center',
-        marginTop:20
-    },
-    FlatlistView:{
-        flexDirection:'row',
-        backgroundColor:'rgba(255, 255, 255, 1)',
-        marginHorizontal:20,
-        marginTop:5,
-        marginBottom:5,
-        borderRadius:10,
-        shadowColor:'black',
-        shadowOpacity:0.5,
-        elevation:2,
-        paddingVertical:8.5,
-        paddingLeft:13.05,
-        paddingRight:15.5,
-    },
-    LineView:{
-        marginTop:17,
-        borderColor:'rgba(180, 180, 183, 0.3)',
-        borderWidth:1
-    },
-    MoreImage:{
-        position:'absolute',
-        left:20
-
-    },
-    SearchImage:{
-        position:'absolute',
-        right:20
-    },
-    DeleteImage:{
-        position:'absolute',
-        right:15.5,
-        top:16.91
-    },
-    TeamsCountText:{
-        marginTop:10,
-        marginBottom:8,
-        marginLeft:20,
-        color:'rgba(0, 0, 0, 1)',
-        fontWeight:'500',
-        fontSize:16,
-        lineHeight:19.49
-    },
-    TeamNameText:{
-        color:'rgba(0, 0, 0, 1)',
-        fontWeight:'500',
-        fontSize:14,
-        lineHeight:17.05
-    },
-    TeamCountText:{
-        color:'rgba(255, 171, 64, 1)',
-        fontWeight:'400',
-        fontSize:11,
-        lineHeight:13.4
-    }
-})
+  Container: {
+    flex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+  },
+  InnerContainer: {
+    backgroundColor: 'rgba(246, 249, 252, 1)',
+    paddingBottom: 67,
+  },
+  TitleBarView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  FlatlistView: {
+    flexDirection: 'row',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    marginHorizontal: 20,
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    elevation: 2,
+    paddingVertical: 8.5,
+    paddingLeft: 13.05,
+    paddingRight: 15.5,
+  },
+  LineView: {
+    marginTop: 17,
+    borderColor: 'rgba(180, 180, 183, 0.3)',
+    borderWidth: 1,
+  },
+  MoreImage: {
+    position: 'absolute',
+    left: 20,
+  },
+  SearchImage: {
+    position: 'absolute',
+    right: 20,
+  },
+  DeleteImage: {
+    position: 'absolute',
+    right: 15.5,
+    top: 16.91,
+  },
+  TeamsCountText: {
+    marginTop: 10,
+    marginBottom: 8,
+    marginLeft: 20,
+    color: 'rgba(0, 0, 0, 1)',
+    fontWeight: '500',
+    fontSize: 16,
+    lineHeight: 19.49,
+  },
+  TeamNameText: {
+    color: 'rgba(0, 0, 0, 1)',
+    fontWeight: '500',
+    fontSize: 14,
+    lineHeight: 17.05,
+  },
+  TeamCountText: {
+    color: 'rgba(255, 171, 64, 1)',
+    fontWeight: '400',
+    fontSize: 11,
+    lineHeight: 13.4,
+  },
+});
